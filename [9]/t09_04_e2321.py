f067d75e24eb916a5d3d0c02bd98cace0edbaf87
Сторожук Назар <dirhdsevy@gmail.com>
2026-06-19 18:08:30 +0000

    commit

--- [9]/t09_04_e2321.py
+++ [9]/t09_04_e2321.py
@@ -1,13 +1,34 @@
 import sys
+import random
 
-def solve():
-    data = sys.stdin.read().split()
-    if not data:
+sys.setrecursionlimit(20000)
+
+def quick_sort(arr, left, right):
+    if left >= right:
         return
-    n = int(data[0])
-    arr = list(map(int, data[1:n+1]))
-    arr.sort()
-    print(*(arr))
+    
+    pivot = arr[random.randint(left, right)]
+    i, j = left, right
+    
+    while i <= j:
+        while arr[i] < pivot:
+            i += 1
+        while arr[j] > pivot:
+            j -= 1
+        if i <= j:
+            arr[i], arr[j] = arr[j], arr[i]
+            i += 1
+            j -= 1
+            
+    quick_sort(arr, left, j)
+    quick_sort(arr, i, right)
 
-if __name__ == "__main__":
-    solve()
+def main():
+    input_data = sys.stdin.read().split()
+    if len(input_data) > 1:
+        arr = [int(x) for x in input_data[1:]]
+        quick_sort(arr, 0, len(arr) - 1)
+        print(*(arr))
+
+if __name__ == '__main__':
+    main()
--- [9]/t09_05_e4037.py
+++ [9]/t09_05_e4037.py
@@ -1,22 +1,53 @@
 import sys
 
-def solve():
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+
+        merge_sort(L)
+        merge_sort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i][0] <= R[j][0]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+
+def main():
     input_data = sys.stdin.read().split()
     if not input_data:
         return
-    
+        
     n = int(input_data[0])
-    robots = []
+    arr = []
+    idx = 1
     
-    for i in range(n):
-        main_num = input_data[1 + 2*i]
-        aux_num = input_data[2 + 2*i]
-        robots.append((int(main_num), aux_num))
+    for _ in range(n):
+        if idx + 1 < len(input_data):
+            arr.append((int(input_data[idx]), int(input_data[idx+1])))
+            idx += 2
+            
+    merge_sort(arr)
     
-    robots.sort(key=lambda x: x[0])
-    
-    output = [f"{r[0]} {r[1]}" for r in robots]
-    sys.stdout.write("\n".join(output) + "\n")
+    for item in arr:
+        print(f"{item[0]} {item[1]}")
 
-if __name__ == "__main__":
-    solve()
+if __name__ == '__main__':
+    main()
